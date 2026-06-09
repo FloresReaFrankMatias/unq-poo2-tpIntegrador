a9d62c5cc5e820cf934492af5a147409593b7835
FloresReaFrankMatias <matiasfloresrea@gmail.com>
2026-06-09 19:33:30 -0300

    add item paquete

---
+++ src/main/java/ar/edu/unq/poo2/item/Paquete.java
@@ -0,0 +1,67 @@
+package ar.edu.unq.poo2.item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paquete implements Item {
+	private String nombre;
+	private double descuento;
+	private List<Item> items;
+	
+	
+	public Paquete(String nombre,double descuento) {
+		// TODO Auto-generated constructor stub
+		this.nombre = nombre;
+		this.descuento=descuento;
+		this.items = new ArrayList<>();
+	}
+
+	@Override
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return nombre;
+	}
+
+	@Override
+	public String getDescripcion() {
+		// TODO Auto-generated method stub
+		return "Paquete: " + this.nombre + "\n" +
+			   "Contiene: " + this.items.size() + " items" + "\n" + 
+		       "Descuento: " + (this.descuento * 100) + "%";
+	}
+
+	
+	public double getPrecioBase() {
+		// TODO Auto-generated method stub
+		return this.items.stream()
+				 .mapToDouble(item -> item.getPrecioBaseCalculado())
+				 .sum();
+	}
+	@Override
+	public double getPrecioBaseCalculado() {
+		 
+		return this.getPrecioBase()* (1.0 - this.descuento);
+	}
+
+	@Override
+	public void add(Item item) {
+		// TODO Auto-generated method stub
+		this.items.add(item);
+
+	}
+
+	@Override
+	public void remove(Item item) {
+		// TODO Auto-generated method stub
+		this.puedeEliminarItem(item);
+		this.items.remove(item);
+	}
+
+	private void puedeEliminarItem(Item item) {
+		// TODO Auto-generated method stub
+		if (!this.items.contains(item)) {
+			throw new RuntimeException("El item no se encuentra en el paquete");
+		}
+	}
+
+}
