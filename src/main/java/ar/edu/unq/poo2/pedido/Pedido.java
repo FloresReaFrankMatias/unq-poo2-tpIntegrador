a27210ef95cc76c58c7212cf77c3875316dd50f2
facundo-lz <lezcano.facundo@outlook.com>
2026-06-09 21:17:28 -0300

    se agrega base de clase Pedido.

---
+++ src/main/java/ar/edu/unq/poo2/pedido/Pedido.java
@@ -0,0 +1,42 @@
+package ar.edu.unq.poo2.pedido;
+
+import ar.edu.unq.poo2.item.Item;
+import java.util.List;
+
+public class Pedido {
+    EstadoPedido estadoActual;
+    List<Item> contenidoDePedido;
+    // Envio envio;
+
+    public void setEstadoActual(EstadoPedido estadoPedido){
+        estadoActual = estadoPedido;
+    }
+
+    public void agregarItem(Item item){
+        estadoActual.agregarItem(this, item);
+    }
+
+    public void quitarItem(Item item){
+        estadoActual.agregarItem(this, item);
+    }
+
+    public void confirmar(){
+        estadoActual.confirmar(this);
+    }
+
+    public void cancelar(){
+        estadoActual.cancelar(this);
+    }
+
+    public void preparar(){
+        estadoActual.preparar(this);
+    }
+
+    public void enviar(){
+        estadoActual.enviar(this);
+    }
+
+    public void entregar(){
+        estadoActual.entregar(this);
+    }
+}
