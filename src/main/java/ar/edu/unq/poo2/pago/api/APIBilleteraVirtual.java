10933e6d99343c557a375c411b5e851fd5f76805
ScheffelaarGerman <gscheffelaar@gmail.com>
2026-06-10 17:42:29 -0300

    Se agregan  operaciones de las APIs de medios de pago

--- src/main/java/ar/edu/unq/poo2/pago/api/APIBilleteraVirtual.java
+++ src/main/java/ar/edu/unq/poo2/pago/api/APIBilleteraVirtual.java
@@ -1,5 +1,13 @@
 package ar.edu.unq.poo2.pago.api;
 
 public interface APIBilleteraVirtual {
+	
+	public abstract boolean validarSaldo();
+
+	public abstract void bloquearSaldo();
+
+	public abstract void acreditarFondos();
+
+	public abstract void enviarPush(String mensaje);
 
 }
--- src/main/java/ar/edu/unq/poo2/pago/api/APITransferenciaBancaria.java
+++ src/main/java/ar/edu/unq/poo2/pago/api/APITransferenciaBancaria.java
@@ -1,5 +1,9 @@
 package ar.edu.unq.poo2.pago.api;
 
 public interface APITransferenciaBancaria {
+	
+	public abstract boolean validarCuenta(String cbu, String alias);
+
+	public abstract void ejecutarTransferencia();
 
 }
