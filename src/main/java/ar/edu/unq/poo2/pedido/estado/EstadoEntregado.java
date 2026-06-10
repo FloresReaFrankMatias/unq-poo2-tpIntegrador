9b4c4cf9fb96c4cffe5cd46948ce35ad388206db
facundo-lz <lezcano.facundo@outlook.com>
2026-06-09 22:25:37 -0300

    Se agrega EstadoEntregado.

---
+++ src/main/java/ar/edu/unq/poo2/pedido/estado/EstadoEntregado.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.poo2.pedido.estado;
+
+import ar.edu.unq.poo2.pedido.Pedido;
+
+public class EstadoEntregado extends EstadoPedido{
+    @Override
+    public void cancelar(Pedido pedido){}
+}
