096cc0d17dd8a503dc3011f19a31b4615bd85c52
ScheffelaarGerman <gscheffelaar@gmail.com>
2026-06-10 11:08:27 -0300

    Se implementa el Template Method procesarPago

--- src/main/java/ar/edu/unq/poo2/pago/MedioPago.java
+++ src/main/java/ar/edu/unq/poo2/pago/MedioPago.java
@@ -1,5 +1,25 @@
 package ar.edu.unq.poo2.pago;
 
 public abstract class MedioPago {
+	
+	
+		// Template Method
+	public void procesarPago(){
+		validarDatos();
+		reservarFondos();
+		ejecutarTransaccion();
+		notificarResultado();
+	}
+	
+		// Primitive Operations
+	protected abstract void validarDatos();
+	protected abstract void reservarFondos();
+	protected abstract void ejecutarTransaccion();
+	
+	  // Hook Method
+	protected  void notificarResultado() {
+		
+	}
+	
 
 }
