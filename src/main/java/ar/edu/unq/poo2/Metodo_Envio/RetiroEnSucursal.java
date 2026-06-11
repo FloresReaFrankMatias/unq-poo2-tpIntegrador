b8a1e50529bb5e0501aae3f8f6bf8339318a5082
FloresReaFrankMatias <matiasfloresrea@gmail.com>
2026-06-11 12:04:40 -0300

    add strategy retiroEnSucursal y inteface Sucursal

---
+++ src/main/java/ar/edu/unq/poo2/Metodo_Envio/RetiroEnSucursal.java
@@ -0,0 +1,22 @@
+package ar.edu.unq.poo2.Metodo_Envio;
+
+public class RetiroEnSucursal implements MetodoDeEnvio {
+	private Sucursal sucursal;
+	public RetiroEnSucursal(Sucursal sucursal) {
+		// TODO Auto-generated constructor stub
+		this.sucursal = sucursal;
+	}
+
+	@Override
+	public int calcularDiasDeEntrega(Pedido pedido) {
+		// TODO Auto-generated method stub
+		return sucursal.hayStock(pedido) ? 0 : 3;
+	}
+
+	@Override
+	public double calcularCosto(Pedido pedido) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/ar/edu/unq/poo2/Metodo_Envio/Sucursal.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.poo2.Metodo_Envio;
+
+public interface Sucursal {
+	public boolean hayStock(Pedido pedido);
+}
