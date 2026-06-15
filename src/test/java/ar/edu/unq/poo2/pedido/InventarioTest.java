957f47d2d3690e08cba3374b812052644bb2cdc0
facundo-lz <lezcano.facundo@outlook.com>
2026-06-15 03:38:35 -0300

    Se agrega versión inicial de los test para la clase Inventario.

---
+++ src/test/java/ar/edu/unq/poo2/pedido/InventarioTest.java
@@ -0,0 +1,64 @@
+package ar.edu.unq.poo2.pedido;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class InventarioTest {
+    Inventario inventario;
+
+    @BeforeEach
+    void setUp(){
+         inventario = new Inventario();
+    }
+
+    @Test
+    public void seIncrementaStockDeProducto(){
+        Map<String, Integer> productoYCantidad = Map.of("Producto", 2);
+        inventario.incrementarStock(productoYCantidad);
+        assertDoesNotThrow(() -> inventario.decrementarStock(productoYCantidad));
+    }
+
+    @Test
+    public void seDecrementaStockDeProducto(){
+        Map<String, Integer> productoYCantidad = Map.of("Producto", 2);
+        inventario.incrementarStock(productoYCantidad);
+        inventario.decrementarStock(productoYCantidad);
+        assertThrows(RuntimeException.class, () -> inventario.decrementarStock(Map.of("Producto", 1)));
+    }
+
+    @Test
+    public void noSePuedeIncrementarEnCantidadNegativaElStockDeUnProducto(){
+        Map<String, Integer> productoYCantidadNegativa = Map.of("Producto", -1);
+        assertThrows(RuntimeException.class, () -> inventario.incrementarStock(productoYCantidadNegativa));
+    }
+
+    @Test
+    public void noSePuedeIncrementarEnCantidadCeroElStockDeUnProducto(){
+        Map<String, Integer> productoYCantidadNegativa = Map.of("Producto", 0);
+        assertThrows(RuntimeException.class, () -> inventario.incrementarStock(productoYCantidadNegativa));
+    }
+
+    @Test
+    public void noSePuedeDecrementarElStockDeUnProductoConCantidadMayorALaDisponible(){
+        Map<String, Integer> productoYCantidad = Map.of("Producto", 2);
+        inventario.incrementarStock(productoYCantidad);
+        assertThrows(RuntimeException.class, () -> inventario.decrementarStock(Map.of("Producto", 3)));
+    }
+
+    @Test
+    public void noSePuedeDecrementarEnCantidadNegativaElStockDeUnProducto(){
+        Map<String, Integer> productoYCantidadNegativa = Map.of("Producto", -1);
+        assertThrows(RuntimeException.class, () -> inventario.decrementarStock(productoYCantidadNegativa));
+    }
+
+    @Test
+    public void noSePuedeDecrementarEnCantidadCeroElStockDeUnProducto(){
+        Map<String, Integer> productoYCantidadNegativa = Map.of("Producto", 0);
+        assertThrows(RuntimeException.class, () -> inventario.decrementarStock(productoYCantidadNegativa));
+    }
+}
