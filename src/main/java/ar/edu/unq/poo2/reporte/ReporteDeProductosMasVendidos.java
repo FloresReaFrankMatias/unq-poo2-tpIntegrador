ab033503d16a2f617f033577bc9c645bb3e83b40
facundo-lz <lezcano.facundo@outlook.com>
2026-06-18 03:22:38 -0300

    Se agrega clase ReporteDeProductosMasVendidos junto con su implementación.

---
+++ src/main/java/ar/edu/unq/poo2/reporte/ReporteDeProductosMasVendidos.java
@@ -0,0 +1,57 @@
+package ar.edu.unq.poo2.reporte;
+
+import ar.edu.unq.poo2.reporte.formato.FormatoVisitante;
+import ar.edu.unq.poo2.venta.HistorialDeVentas;
+import ar.edu.unq.poo2.venta.RegistroDeItem;
+import ar.edu.unq.poo2.venta.Venta;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ReporteDeProductosMasVendidos implements Reporte {
+    private List<LineaDeReporte> lineasProcesadas;
+
+    public ReporteDeProductosMasVendidos(LocalDate inicio, LocalDate fin, HistorialDeVentas historial) {
+        this.lineasProcesadas = armarReporte(inicio, fin, historial);
+    }
+
+    private List<LineaDeReporte> armarReporte(LocalDate inicio, LocalDate fin, HistorialDeVentas historial) {
+        Map<String, LineaDeReporte> analizadosHastaAhora = new HashMap<>();
+        List<Venta> ventasAAnalizar = historial.getVentasEntre(inicio, fin);
+        ventasAAnalizar.forEach(venta -> analizarVenta(venta, analizadosHastaAhora));
+        return analizadosOrdenados(analizadosHastaAhora);
+    }
+
+    private void analizarVenta(Venta venta, Map<String, LineaDeReporte> analizados){
+        venta.getRegistroDeItems().forEach(registroDeItem -> procesarLinea(registroDeItem, analizados));
+    }
+
+    private void procesarLinea(RegistroDeItem registro, Map<String, LineaDeReporte> analizados) {
+        String nombreItemVendido = registro.getNombreItem();
+        analizados.putIfAbsent(nombreItemVendido, new LineaDeReporte(nombreItemVendido));
+        analizados.get(nombreItemVendido).acumular(registro.getPrecio());
+    }
+
+
+    private List<LineaDeReporte> analizadosOrdenados(Map<String, LineaDeReporte> analizados){
+        return analizados.values().stream()
+                .sorted(comparadorPorMasVendidos())
+                .toList();
+    }
+
+    private Comparator<LineaDeReporte> comparadorPorMasVendidos() {
+        return Comparator.comparingInt(linea -> -linea.getCantidadVendida()); // Negativo para ordenar de mayor a menor.
+    }
+
+    public List<LineaDeReporte> getLineasProcesadas() {
+        return lineasProcesadas;
+    }
+
+    @Override
+    public void aceptar(FormatoVisitante visitante) {
+        visitante.aceptar(this);
+    }
+}
