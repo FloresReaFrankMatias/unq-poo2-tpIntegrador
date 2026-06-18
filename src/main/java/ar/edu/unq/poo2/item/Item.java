61eab1b4ccfb4b37d82e416a15860cc6debae0e8
facundo-lz <lezcano.facundo@outlook.com>
2026-06-18 01:52:40 -0300

    Item agrega metodo para dar RegistroDeItem y vuelve a ser interfaz.

--- src/main/java/ar/edu/unq/poo2/item/Item.java
+++ src/main/java/ar/edu/unq/poo2/item/Item.java
@@ -1,16 +1,18 @@
 package ar.edu.unq.poo2.item;
 
+import ar.edu.unq.poo2.venta.RegistroDeItem;
+import java.util.List;
 import java.util.Map;
 
-public abstract class Item {
-	public Map<String, Double> getResumenDePrecio(){
+public interface Item {
+	String getNombre();
+	String getDescripcion();
+	double getPrecioBaseCalculado();
+	void add(Item item);
+	void remove(Item item);
+	Map<String, Integer> getResumenDeSku();
+	List<RegistroDeItem> getRegistroDeItem(double multiplicadorDescuento);
+	default Map<String, Double> getResumenDePrecio(){
 		return Map.of(getNombre(), getPrecioBaseCalculado());
 	}
-
-	public abstract String getNombre();
-	public abstract String getDescripcion();
-	public abstract double getPrecioBaseCalculado();
-	public abstract void add(Item item);
-	public abstract void remove(Item item);
-	public abstract Map<String, Integer> getResumenDeSku();
 }
