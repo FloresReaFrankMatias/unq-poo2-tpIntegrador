8a37202a8cdccd341fa000118780c50fac5f6198
ScheffelaarGerman <gscheffelaar@gmail.com>
2026-06-19 16:20:27 -0300

    Se implmenta busqueda PorPrecioMaximo

--- src/main/java/ar/edu/unq/poo2/busqueda/PorPrecioMaximo.java
+++ src/main/java/ar/edu/unq/poo2/busqueda/PorPrecioMaximo.java
@@ -1,5 +1,17 @@
 package ar.edu.unq.poo2.busqueda;
 
-public class PorPrecioMaximo {
+import ar.edu.unq.poo2.item.Item;
 
+public class PorPrecioMaximo implements CriterioBusqueda{
+	
+	private double precioMaximo;
+	
+    public PorPrecioMaximo(double precioMaximo) {
+	        this.precioMaximo = precioMaximo;
+	}
+    
+    @Override
+    public boolean cumple(Item item) {
+        return item.getPrecioBaseCalculado() <= precioMaximo;
+    }
 }
