ab384b6ecdf51e92a8698d8ed2819b9efba2d8e5
ScheffelaarGerman <gscheffelaar@gmail.com>
2026-06-19 17:54:32 -0300

    Se implementa buesqueda PorDisponibilidad

--- src/main/java/ar/edu/unq/poo2/busqueda/ConsultaDisponibilidadInventario.java
+++ src/main/java/ar/edu/unq/poo2/busqueda/ConsultaDisponibilidadInventario.java
@@ -1,5 +1,18 @@
 package ar.edu.unq.poo2.busqueda;
 
+import ar.edu.unq.poo2.pedido.Inventario;
+
 public class ConsultaDisponibilidadInventario {
 
+    private Inventario inventario;
+
+    public ConsultaDisponibilidadInventario(
+            Inventario inventario) {
+
+        this.inventario = inventario;
+    }
+
+    public boolean tieneStock(String sku) {
+        return inventario.tieneStock(sku);
+    }
 }
--- src/main/java/ar/edu/unq/poo2/busqueda/PorDisponibilidad.java
+++ src/main/java/ar/edu/unq/poo2/busqueda/PorDisponibilidad.java
@@ -1,5 +1,22 @@
 package ar.edu.unq.poo2.busqueda;
 
-public class PorDisponibilidad {
+import ar.edu.unq.poo2.item.Item;
 
+public class PorDisponibilidad implements CriterioBusqueda {
+
+    private ConsultaDisponibilidadInventario consultaInventario;
+
+    public PorDisponibilidad(ConsultaDisponibilidadInventario consultaInventario) {
+
+        this.consultaInventario = consultaInventario;
+    }
+
+    @Override
+    public boolean cumple(Item item) {
+
+        return item.getResumenDeSku()
+                   .keySet()
+                   .stream()
+                   .allMatch(consultaInventario::tieneStock);
+    }
 }
