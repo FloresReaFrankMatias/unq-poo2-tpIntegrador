d014da7a5cc2d5d620847fbb739b623508ee8062
ScheffelaarGerman <gscheffelaar@gmail.com>
2026-06-20 10:05:45 -0300

    Se implementa criterio Not

--- src/main/java/ar/edu/unq/poo2/busqueda/Not.java
+++ src/main/java/ar/edu/unq/poo2/busqueda/Not.java
@@ -1,5 +1,19 @@
 package ar.edu.unq.poo2.busqueda;
 
-public class Not {
+import ar.edu.unq.poo2.item.Item;
 
+public class Not implements CriterioBusqueda {
+
+    private CriterioBusqueda criterio;
+
+    public Not(CriterioBusqueda criterio) {
+
+        this.criterio = criterio;
+    }
+
+    @Override
+    public boolean cumple(Item item) {
+
+        return !criterio.cumple(item);
+    }
 }
