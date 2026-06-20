1d4b9bfca1a2a58f41444e8b92e18ff3013d31ef
ScheffelaarGerman <gscheffelaar@gmail.com>
2026-06-20 09:59:59 -0300

    Se implmenta criterio OR

--- src/main/java/ar/edu/unq/poo2/busqueda/Or.java
+++ src/main/java/ar/edu/unq/poo2/busqueda/Or.java
@@ -1,5 +1,23 @@
 package ar.edu.unq.poo2.busqueda;
 
-public class Or {
+import ar.edu.unq.poo2.item.Item;
 
+public class Or implements CriterioBusqueda {
+
+    private CriterioBusqueda criterio1;
+    private CriterioBusqueda criterio2;
+
+    public Or(CriterioBusqueda criterio1,
+              CriterioBusqueda criterio2) {
+
+        this.criterio1 = criterio1;
+        this.criterio2 = criterio2;
+    }
+
+    @Override
+    public boolean cumple(Item item) {
+
+        return criterio1.cumple(item)
+            || criterio2.cumple(item);
+    }
 }
