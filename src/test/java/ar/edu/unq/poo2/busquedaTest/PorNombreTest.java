47bcbfd9ed0b20e86cf76a9c3d771570076e3108
ScheffelaarGerman <gscheffelaar@gmail.com>
2026-06-20 10:36:13 -0300

    Se adapta búsqueda por nombre al Composite

--- src/test/java/ar/edu/unq/poo2/busquedaTest/PorNombreTest.java
+++ src/test/java/ar/edu/unq/poo2/busquedaTest/PorNombreTest.java
@@ -1,56 +1,91 @@
 package ar.edu.unq.poo2.busquedaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ar.edu.unq.poo2.busqueda.PorNombre;
-import ar.edu.unq.poo2.item.Item;
+import ar.edu.unq.poo2.item.Categoria;
+import ar.edu.unq.poo2.item.Paquete;
+import ar.edu.unq.poo2.item.Producto;
 
 public class PorNombreTest {
 
-    private Item item;
-    private PorNombre criterio;
+    private Producto mate;
+    private Producto bombilla;
+    private Paquete paquete;
 
     @BeforeEach
     void setUp() {
-        item = mock(Item.class);
+
+        mate = new Producto(
+                "SKU1",
+                "Mate Stanley",
+                "Mate térmico",
+                1,
+                "Stanley",
+                Categoria.DEPORTES,
+                10000,
+                0);
+
+        bombilla = new Producto(
+                "SKU2",
+                "Bombilla Pico de Loro",
+                "Bombilla de acero",
+                1,
+                "Lumilagro",
+                Categoria.DEPORTES,
+                2000,
+                0);
+
+        paquete = new Paquete(
+                "Pack Matero",
+                0,
+                "Kit para tomar mate");
+
+        paquete.add(mate);
+        paquete.add(bombilla);
     }
 
     @Test
-    void encuentraCoincidenciaPorNombre() {
+    void encuentraCoincidenciaEnElNombreDeUnProducto() {
 
-        when(item.getNombre()).thenReturn("Mate Stanley");
+        PorNombre criterio = new PorNombre("stanley");
 
-        criterio = new PorNombre("stanley");
-
-        assertTrue(criterio.cumple(item));
+        assertTrue(criterio.cumple(mate));
     }
-    
+
+    @Test
+    void encuentraCoincidenciaEnElNombreDelPaquete() {
+
+        PorNombre criterio = new PorNombre("matero");
+
+        assertTrue(criterio.cumple(paquete));
+    }
+
+    @Test
+    void encuentraCoincidenciaEnUnProductoContenidoEnUnPaquete() {
+
+        PorNombre criterio = new PorNombre("bombilla");
+
+        assertTrue(criterio.cumple(paquete));
+    }
+
     @Test
     void ignoraMayusculasYMinusculas() {
 
-        when(item.getNombre()).thenReturn("Mate Stanley");
+        PorNombre criterio = new PorNombre("STANLEY");
 
-        criterio = new PorNombre("STANLEY");
-
-        assertTrue(criterio.cumple(item));
+        assertTrue(criterio.cumple(paquete));
     }
-   
 
     @Test
-    void retornaFalseCuandoNoHayCoincidencia() {
+    void noEncuentraCoincidenciaCuandoNiElPaqueteNiSusItemsCoinciden() {
 
-        when(item.getNombre()).thenReturn("Mate Stanley");
+        PorNombre criterio = new PorNombre("libro");
 
-        criterio = new PorNombre("bombilla");
-
-        assertFalse(criterio.cumple(item));
+        assertFalse(criterio.cumple(paquete));
     }
-    
-    
-
-  
 }
