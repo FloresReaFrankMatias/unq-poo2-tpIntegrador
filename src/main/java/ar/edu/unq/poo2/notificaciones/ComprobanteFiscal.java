d3bc65f00fea33db97595a2140d00798022638e4
FloresReaFrankMatias <matiasfloresrea@gmail.com>
2026-06-26 15:38:31 -0300

    Agrego implementacion del GeneradorDeFactura y uan interface ComprobanteFiscal

---
+++ src/main/java/ar/edu/unq/poo2/notificaciones/ComprobanteFiscal.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.poo2.notificaciones;
+
+import ar.edu.unq.poo2.pedido.Pedido;
+
+public interface ComprobanteFiscal {
+	public void generarComprobante(Pedido p);
+
+}
--- src/main/java/ar/edu/unq/poo2/notificaciones/MailSender.java
+++ src/main/java/ar/edu/unq/poo2/notificaciones/MailSender.java
@@ -1,6 +1,5 @@
 package ar.edu.unq.poo2.notificaciones;
 
-import ar.edu.unq.poo2.envio.Direccion;
 
 public interface MailSender {
 	public void enviarMail(String direcciónDestino,String  título, String mensaje, String adjunto);
