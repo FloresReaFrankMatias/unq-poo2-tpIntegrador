4f8641789b3d928da08136f14b8c2c69b372c2ea
facundo-lz <lezcano.facundo@outlook.com>
2026-07-01 20:41:46 -0300

    Se agregan tests de clase Cliente.

---
+++ src/test/java/ar/edu/unq/poo2/pedido/ClienteTest.java
@@ -0,0 +1,29 @@
+package ar.edu.unq.poo2.pedido;
+
+import ar.edu.unq.poo2.envio.Direccion;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class ClienteTest {
+    Cliente cliente;
+    Direccion direccionMock;
+
+    @BeforeEach
+    void setUp(){
+        direccionMock = mock(Direccion.class);
+        cliente = new Cliente("cliente@gmail.com", direccionMock);
+    }
+
+    @Test
+    void clienteDevuelveCorreoElectronicoEsperado(){
+        assertEquals("cliente@gmail.com", cliente.getEmail());
+    }
+
+    @Test
+    void clienteDevuelveDireccionEsperada(){
+        assertEquals(direccionMock, cliente.getDireccion());
+    }
+}
